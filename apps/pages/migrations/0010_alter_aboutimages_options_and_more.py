44f555739ec94270cd8b90ca1401df31b5b9336a
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-08-24 12:36:40 +0600

    about page

--- apps/pages/migrations/0010_alter_aboutimages_options_and_more.py
+++ apps/pages/migrations/0010_alter_aboutimages_options_and_more.py
@@ -20,10 +20,6 @@ class Migration(migrations.Migration):
             options={'verbose_name': 'Контактная информация', 'verbose_name_plural': 'Контактная информация'},
         ),
         migrations.RemoveField(
-            model_name='aboutimages',
-            name='page',
-        ),
-        migrations.RemoveField(
             model_name='contactinformation',
             name='working_hours_weekend',
         ),
