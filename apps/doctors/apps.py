ff5ec2332e998912ebbd7c7d619c71bfafa36f5b
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-08-26 17:17:36 +0600

    about page

--- apps/doctors/apps.py
+++ apps/doctors/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class DoctorsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.doctors'
+    verbose_name = 'Специалисты'
+
